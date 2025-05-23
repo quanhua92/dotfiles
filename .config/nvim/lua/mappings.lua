fa158111bfbee863d48411f0f0dca4ad2f053ba3
Quan Hua <quanhua92@gmail.com>
2025-05-23 14:45:07 +0700

    feat: add vim mapping leader ca

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,6 +4,7 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+map("n", "<leader>ca", "<cmd>%bd<CR>", { desc = "Close all buffers" })
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
