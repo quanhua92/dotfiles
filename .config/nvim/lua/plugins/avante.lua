f3af77876910ad0843148a64572c571be922f0d3
Quan Hua <quanhua92@gmail.com>
2025-06-18 14:26:31 +0700

    disable render-markdown

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -66,9 +66,15 @@ return {
       -- Make sure to set this up properly if you have lazy=true
       "MeanderingProgrammer/render-markdown.nvim",
       opts = {
-        file_types = { "markdown", "Avante" },
+        file_types = {
+          -- "markdown",
+          "Avante",
+        },
       },
-      ft = { "markdown", "Avante" },
+      ft = {
+        -- "markdown",
+        "Avante",
+      },
     },
   },
 }
